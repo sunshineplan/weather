e5834df30ca2f5392ab768230500779c7510e138
sunshineplan <sunshineplan@users.noreply.github.com>
2023-11-01 11:44:52 +0800

    server: Record three day before

--- server/run.go
+++ server/run.go
@@ -85,7 +85,7 @@ func run() error {
 
 	run := scheduler.NewScheduler
 	run().At(scheduler.ScheduleFromString(*dailyReport)).Do(daily)
-	run().At(scheduler.HourSchedule(9, 16, 23)).Do(func(t time.Time) { record(t.AddDate(0, 0, -1)) })
+	run().At(scheduler.HourSchedule(9, 16, 23)).Do(func(t time.Time) { record(t.AddDate(0, 0, -3)) })
 	run().At(scheduler.ClockSchedule(scheduler.ClockFromString(*start), scheduler.ClockFromString(*end), *interval)).Do(alert)
 	run().At(scheduler.MinuteSchedule(0, 30)).Do(func(t time.Time) { zoomEarth(t, false) })
 
