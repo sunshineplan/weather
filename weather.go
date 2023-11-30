861f5f66336fcee35e2a525ec1af8b250b61d9dc
sunshineplan <sunshineplan@users.noreply.github.com>
2023-11-30 14:54:20 +0800

    coordinates: Add GeoLocator interface

--- weather.go
+++ weather.go
@@ -7,7 +7,7 @@ import (
 )
 
 type API interface {
-	Coordinates(string) (coordinates.Coordinates, error)
+	coordinates.GeoLocator
 	Realtime(string) (Current, error)
 	Forecast(string, int) ([]Day, error)
 	History(string, time.Time) (Day, error)
