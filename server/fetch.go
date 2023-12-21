e0b8fb54d83f74d6ac7a7f29e294619f7dbfe1de
sunshineplan <sunshineplan@users.noreply.github.com>
2023-12-21 14:16:06 +0800

    server: Concurrent fetch (#69)

--- server/fetch.go
+++ server/fetch.go
@@ -11,27 +11,38 @@ import (
 )
 
 func getWeather(query string, n int, t time.Time, realtime bool) (current weather.Current, days []weather.Day, err error) {
-	if realtime {
-		current, err = forecast.Realtime(query)
+	c := make(chan error, 3)
+	go func() {
+		var err error
+		if realtime {
+			current, err = forecast.Realtime(query)
+		}
+		c <- err
+	}()
+	var forecasts []weather.Day
+	go func() {
+		var err error
+		forecasts, err = forecast.Forecast(query, n)
 		if err != nil {
+			c <- err
+		} else if len(forecasts) < n {
+			c <- fmt.Errorf("bad forecast number: %d", len(forecasts))
+		} else if date := t.Format("01-02"); !strings.HasSuffix(forecasts[0].Date, date) {
+			c <- fmt.Errorf("the first forecast date(%s) does not match the input(%s)", forecasts[0].Date, date)
+		} else {
+			c <- nil
+		}
+	}()
+	var yesterday weather.Day
+	go func() {
+		var err error
+		yesterday, err = history.History(query, t.AddDate(0, 0, -1))
+		c <- err
+	}()
+	for i := 0; i < 3; i++ {
+		if err = <-c; err != nil {
 			return
 		}
-	}
-	forecasts, err := forecast.Forecast(query, n)
-	if err != nil {
-		return
-	}
-	if len(forecasts) < n {
-		err = fmt.Errorf("bad forecast number: %d", len(forecasts))
-		return
-	}
-	if date := t.Format("01-02"); !strings.HasSuffix(forecasts[0].Date, date) {
-		err = fmt.Errorf("the first forecast date(%s) does not match the input(%s)", forecasts[0].Date, date)
-		return
-	}
-	yesterday, err := history.History(query, t.AddDate(0, 0, -1))
-	if err != nil {
-		return
 	}
 	days = append([]weather.Day{yesterday}, forecasts...)
 	return
@@ -49,16 +60,30 @@ func getAQI(aqiType aqi.Type, q string) (aqi.Current, error) {
 	return res, err
 }
 
-func getAll(q string, n int, aqiType aqi.Type, t time.Time, realtime bool) (
-	current weather.Current, days []weather.Day, avg weather.Day, currentAQI aqi.Current, err error) {
-	if current, days, err = getWeather(q, n, t, realtime); err != nil {
-		return
-	}
-	if q == *query {
-		if avg, err = average(t.Format("01-02"), 2); err != nil {
+func getAll(q string, n int, aqiType aqi.Type, t time.Time, realtime bool,
+) (current weather.Current, days []weather.Day, avg weather.Day, currentAQI aqi.Current, err error) {
+	c := make(chan error, 3)
+	go func() {
+		var err error
+		current, days, err = getWeather(q, n, t, realtime)
+		c <- err
+	}()
+	go func() {
+		var err error
+		if q == *query {
+			avg, err = average(t.Format("01-02"), 2)
+		}
+		c <- err
+	}()
+	go func() {
+		var err error
+		currentAQI, err = getAQI(aqiType, q)
+		c <- err
+	}()
+	for i := 0; i < 3; i++ {
+		if err = <-c; err != nil {
 			return
 		}
 	}
-	currentAQI, err = getAQI(aqiType, q)
 	return
 }
