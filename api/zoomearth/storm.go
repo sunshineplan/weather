2131b47be947617c50f94a362eb66c9916aac9cd
sunshineplan <sunshineplan@users.noreply.github.com>
2024-05-27 22:02:39 +0800

    storm: Coordinates according time

--- api/zoomearth/storm.go
+++ api/zoomearth/storm.go
@@ -17,49 +17,42 @@ func (id StormID) URL() string {
 	return fmt.Sprintf("%s/storms/%s/", root, id)
 }
 
+type stormData struct {
+	ID     StormID
+	Season string
+	Name   string
+	Title  string
+	Active bool
+	Type   string
+	Place  string
+	Cone   []coordinates.LongLat
+	Track  []track
+	JA     int
+}
+
+var _ storm.Track = track{}
+
+type track struct {
+	D time.Time           `json:"date"`
+	C coordinates.LongLat `json:"coordinates"`
+	F bool                `json:"forecast"`
+}
+
+func (t track) Date() time.Time                      { return t.D }
+func (t track) Coordinates() coordinates.Coordinates { return t.C }
+func (t track) Forecast() bool                       { return t.F }
+
 func (id StormID) Data() (storm.Data, error) {
 	resp, err := http.Get(fmt.Sprint(root, "/data/storms/?id=", id))
 	if err != nil {
 		return storm.Data{}, err
 	}
 	defer resp.Body.Close()
-	var data StormData
+	var data stormData
 	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
 		return storm.Data{}, err
 	}
-	data.calcCoordinates()
 	return data.Convert(), nil
-}
-
-func calcCoordinates(a, b Track, t time.Time) coordinates.Coordinates {
-	start, end, n := a.Date.Unix(), b.Date.Unix(), t.Unix()
-	if n < start {
-		return a.Coordinates
-	} else if n > end {
-		return b.Coordinates
-	}
-	rate := float64(n-start) / float64(end-start)
-	return coordinates.LongLat{
-		a.Coordinates[0] + (b.Coordinates[0]-a.Coordinates[0])*rate,
-		a.Coordinates[1] + (b.Coordinates[1]-a.Coordinates[1])*rate,
-	}
-}
-
-func (data *StormData) calcCoordinates() {
-	var a, b Track
-	for _, i := range data.Track {
-		if !i.Forecast {
-			a = i
-		} else {
-			b = i
-			break
-		}
-	}
-	if b.Coordinates == [2]float64{} {
-		data.Coordinates = a.Coordinates
-	} else {
-		data.Coordinates = calcCoordinates(a, b, time.Now())
-	}
 }
 
 func GetStorms(t time.Time) (storms []storm.Storm, err error) {
--- api/zoomearth/types.go
+++ api/zoomearth/types.go
@@ -1,34 +1,10 @@
 package zoomearth
 
-import (
-	"time"
-
-	"github.com/sunshineplan/weather/unit/coordinates"
-)
+import "time"
 
 type ZoomEarthAPI struct{}
 
 type StormID string
-
-type StormData struct {
-	ID     StormID
-	Name   string
-	Title  string
-	Active bool
-	Type   string
-	Place  string
-	Cone   []coordinates.LongLat
-	Track  []Track
-	JA     int
-
-	Coordinates coordinates.Coordinates
-}
-
-type Track struct {
-	Date        time.Time
-	Coordinates coordinates.LongLat
-	Forecast    bool
-}
 
 type MapOptions struct {
 	width    int
